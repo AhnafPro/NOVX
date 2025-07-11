0f3dd2dc4c5f902a3471c7f6aad11fdf46df3ed3
Ahnaf <93572244+AhnafPro@users.noreply.github.com>
2025-07-11 17:15:29 +0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,18 +1,25 @@
-const { ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
-window.addEventListener("contextmenu", e => {
+contextBridge.exposeInMainWorld("electronAPI", {
+  windowControl: (action) => ipcRenderer.send("win:" + action),
+  showContextMenu: (details) => ipcRenderer.send("show-context-menu", details),
+  hideContextMenu: () => ipcRenderer.send("hide-context-menu"),
+  sendSearch: (query) => ipcRenderer.send("save-search", query)
+});
+
+window.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 
   const el = e.target;
   const params = {
-    selectionText: window.getSelection().toString(),
-    mediaSrc: el.tagName === "IMG" || el.tagName === "VIDEO" ? el.src : null,
+    selectionText: window.getSelection().toString().trim() || null,
+    mediaSrc: ["IMG", "VIDEO", "AUDIO"].includes(el.tagName) ? el.src : null,
     linkURL: el.closest("a")?.href || null
   };
 
   ipcRenderer.sendToHost("show-menu", {
-    x: e.pageX,
-    y: e.pageY,
+    x: e.clientX,
+    y: e.clientY,
     params
   });
 });
